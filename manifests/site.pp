6bd53d2ea1cc1421f4ef27a3b3e03bdfda34d05f
William <williamwang198@gmail.com>
2016-03-28 14:27:57 -0700

    Managing /etc/motd

--- manifests/site.pp
+++ manifests/site.pp
@@ -43,4 +43,14 @@ node default {
   # Example:
   #   class { 'my_class': }
   notify { "Hello, my name is ${::hostname}": }
+  
+  file { '/etc/motd':
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0664',
+    content => 'Learned about Puppet!'
+  }
 }
+
+
