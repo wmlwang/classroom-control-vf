0e6738589a027752dcbf543a1b019e983b4f1abc
WANGWIL1 <william.m.wang@intel.com>
2016-03-30 13:58:51 -0700

    v15.1

--- site/users/manifests/managed_user.pp
+++ site/users/manifests/managed_user.pp
@@ -2,10 +2,16 @@ define users::managed_user (
   $group = $title,
   $home_dir = '/home',
   ) {
+    file { $home_dir:
+      ensure => directory,
+    }
+
     user { $title :
       ensure => present,
-      home => $home_dir
+      home => $home_dir,
     }
+
+
 
     file { ["${home_dir}/${title}", "${home_dir}/${title}/.ssh"] :
       ensure => directory,
